c73357c31932bc922366b156decbfa119c187440
Glitch (sab-ring-buffer) <none>
2020-04-15 20:05:28 +0000

    👮👩‍❤️‍💋‍👩 Checkpoint ./script.js:166580/1103

--- script.js
+++ script.js
@@ -10,8 +10,7 @@ class RingBuffer {
     );
     const header = new Uint32Array(buffer, 0, RingBuffer.HEADER_LENGTH);
     const body = new Uint8Array(buffer, RingBuffer.HEADER_LENGTH, length);
-    header.set([0 /* readIndex */, 0 /* writeIndex */], 0);
-
+    
     return new RingBuffer(buffer);
   }
 
@@ -39,47 +38,56 @@ class RingBuffer {
   }
 
   append(data) {
-    for (const byte of data) {
-      const writeIndex = Atomics.load(this._header, RingBuffer.HEADER.WRITE);
-      console.log(writeIndex, byte);
+    Atomics.wait(this._header, RingBuffer.HEADER.WRITE, 0);
+    {
+      // Transaction scoped for my reading.
+      Atomics.set(this._header, RingBuffer.HEADER.WRITING, 1);
+      for (const byte of data) {
+        const writeIndex = Atomics.load(this._header, RingBuffer.HEADER.WRITE);
+        console.log(writeIndex, byte);
 
-      Atomics.store(this._body, writeIndex, byte);
+        Atomics.store(this._body, writeIndex, byte);
 
-      this._writeIndex = Atomics.add(this._header, RingBuffer.HEADER.WRITE, 1);
+        this._writeIndex = Atomics.add(this._header, RingBuffer.HEADER.WRITE, 1);
 
-      if (this._writeIndex == this._length - 1) {
-        console.log("wrap");
-        this._writeIndex = Atomics.store(
-          this._header,
-          RingBuffer.HEADER.WRITE,
-          0
-        );
+        if (this._writeIndex == this._length - 1) {
+          this._writeIndex = Atomics.store(
+            this._header,
+            RingBuffer.HEADER.WRITE,
+            0
+          );
+        }
       }
     }
+    Atomics.set(this._header, RingBuffer.HEADER.WRITING, 0);
   }
 
   // Reads the next byte of data
   read() {
-    const readIndex = Atomics.load(this._header, RingBuffer.HEADER.READ);
-    const writeIndex = Atomics.load(this._header, RingBuffer.HEADER.WRITE);
+    Atomics.wait(this._header, RingBuffer.HEADER.READING, 0);
+    {
+      // Transaction scoped for my reading.
+      Atomics.set(this._header, RingBuffer.HEADER.WRITING, 1);
+      const readIndex = Atomics.load(this._header, RingBuffer.HEADER.READ);
+      const writeIndex = Atomics.load(this._header, RingBuffer.HEADER.WRITE);
 
-    if (readIndex == writeIndex) return undefined;
+      if (readIndex == writeIndex) return undefined;
 
-    const value = Atomics.load(this._body, readIndex);
+      const value = Atomics.load(this._body, readIndex);
 
-    this._readIndex = Atomics.add(this._header, RingBuffer.HEADER.READ, 1);
+      this._readIndex = Atomics.add(this._header, RingBuffer.HEADER.READ, 1);
 
-    if (this._readIndex == this._length - 1) {
-      this._readIndex = Atomics.store(this._header, RingBuffer.HEADER.READ, 0);
+      if (this._readIndex == this._length - 1) {
+        this._readIndex = Atomics.store(this._header, RingBuffer.HEADER.READ, 0);
+      }
     }
-
     return value;
   }
 
-  * readToHead() {
+  * readToHead() { // Feels odd to have to create a buffer the same size as the buffer. Just iterate.
     let data;
     while((data = this.read()) != undefined) {
-      yield data;
+      yield data; 
     }
   }
 
